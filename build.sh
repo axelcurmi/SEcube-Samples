fdfd52aacd17e86f8166ae39da9e4e91a494e922
Axel Curmi <axel.curmi@gmail.com>
2024-01-15 10:36:36 +0100

    Add hello_world

---
+++ build.sh
@@ -0,0 +1,32 @@
+rm -rf {obj,bin}
+mkdir {obj,bin}
+
+g++ -O0 -g3 -c -o obj/L0_base.o SEcubeSources/L0/L0Base/L0_base.cpp
+g++ -O0 -g3 -c -o obj/L0_commodities.o SEcubeSources/L0/L0_commodities.cpp
+g++ -O0 -g3 -c -o obj/L0_communication.o SEcubeSources/L0/L0_communication.cpp
+g++ -O0 -g3 -c -o obj/L0_provision.o SEcubeSources/L0/L0_provision.cpp
+g++ -O0 -g3 -c -o obj/L0.o SEcubeSources/L0/L0.cpp
+g++ -O0 -g3 -c -o obj/aes256.o SEcubeSources/L1/CryptoLibraries/aes256.cpp
+gcc -O0 -g3 -c -o obj/sha256.o SEcubeSources/L1/CryptoLibraries/sha256.c
+gcc -O0 -g3 -c -o obj/pbkdf2.o SEcubeSources/L1/CryptoLibraries/pbkdf2.c
+g++ -O0 -g3 -c -o obj/L1_base.o SEcubeSources/L1/L1Base/L1_base.cpp
+g++ -O0 -g3 -c -o obj/L1_login_logout.o SEcubeSources/L1/L1_login_logout.cpp
+g++ -O0 -g3 -c -o obj/L1_security.o SEcubeSources/L1/L1_security.cpp
+g++ -O0 -g3 -c -o obj/L1.o SEcubeSources/L1/L1.cpp
+
+g++ -O3 -g3 -c -o obj/hello_world.o hello_world.cpp
+
+g++ -o bin/hello_world \
+    obj/L0.o \
+    obj/L0_commodities.o \
+    obj/L0_communication.o \
+    obj/L0_provision.o \
+    obj/L0_base.o \
+    obj/sha256.o \
+    obj/aes256.o \
+    obj/pbkdf2.o \
+    obj/L1_base.o \
+    obj/L1_login_logout.o \
+    obj/L1_security.o \
+    obj/L1.o \
+    obj/hello_world.o
